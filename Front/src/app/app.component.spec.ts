a89fa6754c2dbfbb73c8409bb70ad550825eed1b
Ui Sam <uisam.santos@gmail.com>
2023-01-31 01:58:03 -0300

    corrigindo novo nome onde faltou

--- Front/src/app/app.component.spec.ts
+++ Front/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ProEventos-App'`, () => {
+  it(`should have as title 'Divulga-App'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ProEventos-App');
+    expect(app.title).toEqual('Divulga-App');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ProEventos-App app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Divulga-App app is running!');
   });
 });
