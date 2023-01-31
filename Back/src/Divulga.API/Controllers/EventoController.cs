cda4a31c4cdb715819707d3dee57744c02666184
Ui Sam <uisam.santos@gmail.com>
2023-01-31 00:47:15 -0300

    refact: atualizando dotnet

--- Back/src/Divulga.API/Controllers/EventoController.cs
+++ Back/src/Divulga.API/Controllers/EventoController.cs
@@ -1,34 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Divulga.API.Data;
 using Divulga.API.Models;
 
-namespace Divulga.API.Controllers
+namespace Divulga.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EventosController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class EventosController : ControllerBase
+    private readonly DataContext _context;
+
+    public EventosController(DataContext context)
     {
-        private readonly DataContext _context;
+        _context = context;
+    }
 
-        public EventosController(DataContext context)
-        {
-            _context = context;
-        }
-
-        [HttpGet]
-        public IEnumerable<Evento> Get()
-        {
-            return _context.Eventos;
-        }
-        [HttpGet("{id}")]
-        public Evento Get(int id)
-        {
-            return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
-        }
+    [HttpGet]
+    public IEnumerable<Evento> Get()
+    {
+        return _context.Eventos;
+    }
+    [HttpGet("{id}")]
+    public Evento Get(int id)
+    {
+        return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
     }
 }
--- Back/src/Divulga.API/Models/Evento.cs
+++ Back/src/Divulga.API/Models/Evento.cs
@@ -1,14 +1,13 @@
-namespace Divulga.API.Models
-{
-    public class Evento
-    {
-        public int EventoId { get; set; }
-        public string Local { get; set; }
-        public string DataEvento { get; set; }
-        public string Tema { get; set; }
-        public int QtdPessoas { get; set; }
-        public string Lote { get; set; }
-        public string ImagemUrl { get; set; }
+namespace Divulga.API.Models;
 
-    }
+public class Evento
+{
+    public int EventoId { get; set; }
+    public string Local { get; set; }
+    public string DataEvento { get; set; }
+    public string Tema { get; set; }
+    public int QtdPessoas { get; set; }
+    public string Lote { get; set; }
+    public string ImagemUrl { get; set; }
+
 }
